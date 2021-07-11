764bbf272905b4e7bd622895acff8abca49452e1
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-07-11 12:39:24 +0530

    diagonal works

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -25,21 +25,24 @@ export class GameComponent implements OnInit {
     if(this.player1){
       this.board[row][col] = "X";
       if(this.winningMove(row, col, "X")){
-        alert("Player1 won");
+        setTimeout(()=>{
+          alert("Player1 won");
+        },0)
       }
     }else{
       this.board[row][col] = "O";
       if(this.winningMove(row, col, "O")){
-        alert("Player2 won");
+        setTimeout(()=>{
+          alert("Player2 won");
+        },0)
       }
     }
     this.player1 = !this.player1;
   }
 
   winningMove(row:number, col:number, mark:string){
-    if(row==1 && col==1){
-      if(this.board[0][0]==this.board[1][1] && this.board[2][2]==this.board[1][1]) return true;
-      if(this.board[0][2]==this.board[1][1] && this.board[2][0]==this.board[1][1]) return true;
+    if((row==col || row-col==0) && this.diagonalCheck(row, col,mark)){
+      return true;
     }
     const isMatch = (current:string)=>current === mark;
     const rowWin = this.board[row].every(isMatch);
@@ -47,6 +50,15 @@ export class GameComponent implements OnInit {
     const colums = [this.board[0][col], this.board[1][col],this.board[2][col]];
     const colWin = colums.every(isMatch);
     if(colWin) return colWin;
+    return false;
+  }
+
+  diagonalCheck(row:number, col:number, mark:string){
+    const leadDia = [this.board[0][0], this.board[1][1], this.board[2][2]];
+    const secDia = [this.board[0][2],this.board[1][1], this.board[2][0]];
+    const isMatch = (current:string)=>current === mark;
+    if(leadDia.every(isMatch, mark)) return true;
+    if(secDia.every(isMatch, mark)) return true;
     return false;
   }
 }
